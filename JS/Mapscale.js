f4ffc5fb5ecd452b8650f514fc1afed1424de5dc
Coworker <nosvamrad@gmail.com>
2024-10-08 16:09:54 +0300

    Add files via upload

--- JS/Mapscale.js
+++ JS/Mapscale.js
@@ -17,21 +17,12 @@ function zoomIn() {
     const style1 = getComputedStyle(root1);
     const width = style1.getPropertyValue('--width');
 
-
     const element = document.getElementById("blocksize");
     root1.style.setProperty("--width", element.clientWidth * 1.1 + 'px');
 
-    console.log(pic1.style.top);
-    console.log(pic1.style.left);
 
-    console.log(pic1.offsetTop);
-    console.log(pic1.offsetLeft);
     pic1.style.top = pic1.offsetTop * 1.1 + 'px';
     pic1.style.left = pic1.offsetLeft * 1.1 + 'px';
-
-    console.log(pic1.style.top);
-    console.log(pic1.style.left);
-
 
 
     pic2.style.top = pic2.offsetTop * 1.1 + 'px';
@@ -91,53 +82,20 @@ function zoomOut() {
 
 }
 
+function infoBlock() {
+
+    var instr = document.getElementById('instr1');
+    instr.style.visibility = "visible";
+}
+
+function CloseInfoBlock() {
+
+    var instr = document.getElementById('instr1');
+    instr.style.visibility = "hidden";
+}
 
 
 document.getElementById("plusbutton").addEventListener("click", zoomIn);
 document.getElementById("minusbutton").addEventListener("click", zoomOut);
-
-
-//move map by mouse
-document.addEventListener('DOMContentLoaded', function() {
-    const ele = document.getElementById('mainscroll');
-    ele.style.cursor = 'grab';
-
-    let pos = { top: 0, left: 0, x: 0, y: 0 };
-
-    const mouseDownHandler = function(e) {
-        ele.style.cursor = 'grabbing';
-        ele.style.userSelect = 'none';
-
-        pos = {
-            left: ele.scrollLeft,
-            top: ele.scrollTop,
-            // Get the current mouse position
-            x: e.clientX,
-            y: e.clientY,
-        };
-
-        document.addEventListener('mousemove', mouseMoveHandler);
-        document.addEventListener('mouseup', mouseUpHandler);
-    };
-
-    const mouseMoveHandler = function(e) {
-        // How far the mouse has been moved
-        const dx = e.clientX - pos.x;
-        const dy = e.clientY - pos.y;
-
-        // Scroll the element
-        ele.scrollTop = pos.top - dy;
-        ele.scrollLeft = pos.left - dx;
-    };
-
-    const mouseUpHandler = function() {
-        ele.style.cursor = 'grab';
-        ele.style.removeProperty('user-select');
-
-        document.removeEventListener('mousemove', mouseMoveHandler);
-        document.removeEventListener('mouseup', mouseUpHandler);
-    };
-
-    // Attach the handler
-    ele.addEventListener('mousedown', mouseDownHandler);
-});
+document.getElementById("infobutton").addEventListener("click", infoBlock);
+document.getElementById("okbutton").addEventListener("click", CloseInfoBlock);
